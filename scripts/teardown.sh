2d23b96faf630746f43d2986a68a58a35b96fef5
Moti Asayag <masayag@redhat.com>
2022-03-02 09:25:32 +0200

    Run teardown in parallel
    Signed-off-by: Moti Asayag <masayag@redhat.com>

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-kubectl get edgedevices --all-namespaces --no-headers | awk '{print $2 " --namespace=" $1}' |  xargs -n 2 kubectl patch edgedevice -p '{"metadata":{"finalizers":null}}' --type=merge
-kubectl get edgedeployment --all-namespaces --no-headers | awk '{print $2 " --namespace=" $1}' | xargs -n 2 kubectl patch edgedeployment -p '{"metadata":{"finalizers":null}}' --type=merge
-kubectl delete edgedevice --all --all-namespaces
-kubectl delete edgedeployments --all --all-namespaces
-
 if [ $# -eq 1 ]; then
     for i in $(seq $1)
     do
-      kubectl delete ns $((i - 1))
+      NS=$((i - 1))
+      kubectl get edgedevices -n $NS --no-headers | awk -v ns=$NS '{print $1 " --namespace=" ns}' |  xargs -P 20 -n 2 kubectl patch edgedevice -p '{"metadata":{"finalizers":null}}' --type=merge
+      kubectl get edgedeployment -n $NS --no-headers | awk -v ns=$NS '{print $1 " --namespace=" ns}' | xargs -P 20  -n 2 kubectl patch edgedeployment -p '{"metadata":{"finalizers":null}}' --type=merge
+      kubectl delete edgedevice --all -n $NS
+      kubectl delete edgedeployments --all -n $NS
+      kubectl delete ns $NS
     done
+    exit 0
 fi
+
+kubectl get edgedevices --all-namespaces --no-headers | awk '{print $2 " --namespace=" $1}' |  xargs -P 20 -n 2 kubectl patch edgedevice -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl get edgedeployment --all-namespaces --no-headers | awk '{print $2 " --namespace=" $1}' | xargs -P 20 -n 2 kubectl patch edgedeployment -p '{"metadata":{"finalizers":null}}' --type=merge
+kubectl delete edgedevice --all --all-namespaces
+kubectl delete edgedeployments --all --all-namespaces
